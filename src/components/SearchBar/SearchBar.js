9547d4a3c092b1dc40a1e0d99c1d68f642ae1a8c
yasithWimukthi <it19966922@my.sliit.lk>
2020-12-09 17:55:58 +0530

    Implement SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react'
 export default class SearchBar extends Component {
     render() {
         return (
-            <div>
-                search
+            <div className="search-bar ui segment">
+                <form className="ui form"> 
+                    <div className="field">
+                        <label>Video Search</label>
+                        <input type="text"/>
+                    </div>
+                </form>
             </div>
         )
     }
